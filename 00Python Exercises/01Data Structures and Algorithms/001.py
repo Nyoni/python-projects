4b9f8810fbe0b2560ad9e8768159a9afacd9f4de
Nyoni <tafnyoni123@gmail.com>
2021-09-06 12:50:53 +0200

    python exercises

---
+++ 00Python Exercises/01Data Structures and Algorithms/001.py
@@ -0,0 +1,8 @@
+######################################################
+#### Unpacking a Sequence into Separate Variables ####
+######################################################
+
+#### You have an N-element tuple or sequence that you would like to 
+#### unpack into a collection of N variables.
+
+
